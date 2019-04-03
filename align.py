3a0667bfaf74897876cd8ab4ef889b8872671d0e
henlium <b04902064@ntu.edu.tw>
2019-04-03 22:57:28 +0800

    :bug: Fixed image shifting in align

--- align.py
+++ align.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 
 def gray(img, opt='cv'):
@@ -49,9 +51,9 @@ def imgShift(im, x, y):
     else:
         shifted = im
     if y > 0:
-        shifted = [np.append([False] * y, row[:-y]) for row in shifted]
+        shifted = [np.concatenate([[0, 0, 0] * y, row[:-y]]) for row in shifted]
     elif y < 0:
-        shifted = [np.append(row[-y:], [False] * -y) for row in shifted]
+        shifted = [np.concatenate([row[-y:], [0, 0, 0] * -y]) for row in shifted]
     return shifted
 
 def getExpShift(img0, img1, shiftBits):
@@ -87,10 +89,11 @@ def align(img0, img1, level, opt='cv'):
     return getExpShift(g0, g1, level)
 
 def process(imgs_src, level, opt='cv'):
-    ret = [imgs_src[0]]
+    ret = []
     if len(imgs_src) < 2:
         return ret
     else:
+        ret = [imgs_src[0]]
         for i in range(1, len(imgs_src)):
             x, y = align(imgs_src[0], imgs_src[1], level, opt)
             ret.append(imgShift(imgs_src[i], x, y))
